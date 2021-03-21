af098c016f4d40dd387b63bdde49e894ac2ac386
Greet <vanderlindengreet@gmail.com>
2021-03-21 18:09:06 +0100

    show and hide by click on hamburger

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-// function to show menu whem hamburger icon is clicked
+// function to show and hide menu when hamburger icon is clicked
 
 function showMenu() {
   const hamburgerButton = document.querySelector(".icon-hamburger-icon");
   hamburgerButton.addEventListener("click", () => {
     const subMenuButtons = document.querySelectorAll("#hamburger > li");
-    subMenuButtons.forEach((subMenu) => {
-      subMenu.style.display = "block";
-      subMenu.style.zIndex = "1";
-    });
+    hamburgerButton.classList.toggle("activeHamburger");
+    const activatedHamburger = document.querySelector(".activeHamburger");
+    if (activatedHamburger === null) {
+      subMenuButtons.forEach((subMenu) => {
+        subMenu.style.display = "none";
+        subMenu.style.zIndex = "1";
+      });
+    } else {
+      subMenuButtons.forEach((subMenu) => {
+        subMenu.style.display = "block";
+        subMenu.style.zIndex = "1";
+      });
+    }
   });
 }
 
