0dbd3c9aef58f9400fbc1f94c27d164ef17d97f8
Greet <vanderlindengreet@gmail.com>
2021-03-21 17:55:04 +0100

    mobile menu working with js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function showMenu() {
+  const hamburgerButton = document.querySelector(".icon-hamburger-icon");
+  hamburgerButton.addEventListener("click", () => {
+    const subMenuButtons = document.querySelectorAll("#hamburger > li");
+    subMenuButtons.forEach((subMenu) => {
+      subMenu.style.display = "block";
+      subMenu.style.zIndex = "1";
+    });
+  });
+}
+
+// call showMenu function when DOM loaded
+
+window.addEventListener("DOMContentLoaded", () => {
+  showMenu();
+});
